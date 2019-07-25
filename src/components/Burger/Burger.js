55deacb9bab99a98e330bd8aabe76f04deb95cf3
Yuri Becker <becker.yr@gmail.com>
2019-07-25 17:11:30 -0300

    Add burger stateless component + css

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import styles from './Burger.module.css';
+
+const burger = (props) => {
+  const { ingredients } = props;
+  const transformedIngredients = Object.keys(ingredients).map(ingredientName => [...Array(ingredients[ingredientName])].map((ing, index) => (
+    <BurgerIngredient key={ingredientName + index} type={ingredientName} />
+  )));
+
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+burger.propTypes = {
+  ingredients: PropTypes.object.isRequired
+};
+
+export default burger;
