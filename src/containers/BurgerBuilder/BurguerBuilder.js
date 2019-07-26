2b0b08b181c60360ccbce0fcc5508de8ede34f8e
Yuri Becker <becker.yr@gmail.com>
2019-07-26 18:49:04 -0300

    Using Hooks in BurgerBuilder.js

--- src/containers/BurgerBuilder/BurguerBuilder.js
+++ src/containers/BurgerBuilder/BurguerBuilder.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Burger from '../../components/Burger/Burger';
 
-class BurguerBuilder extends Component {
-  state = {
-    ingredients: {
-      meat: 1,
-      cheese: 2,
-      bacon: 1,
-      salad: 1
-    }
-  };
+function BurguerBuilder(props) {
+  const [ingredients, setIngredients] = useState({
+    meat: 1,
+    cheese: 2,
+    bacon: 1,
+    salad: 1
+  });
 
-  render() {
-    const { ingredients } = this.state;
-    return (
-      <>
-        <Burger ingredients={ingredients} />
-        <div>CONTROLS</div>
-      </>
-    );
-  }
+  return (
+    <>
+      <Burger ingredients={ingredients} />
+      <div>CONTROLS</div>
+    </>
+  );
 }
 
 export default BurguerBuilder;
