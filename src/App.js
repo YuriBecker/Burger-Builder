c5a16e3f6429a420fecc3abbfe678f7d8292df1f
Yuri Becker <becker.yr@gmail.com>
2019-07-26 22:30:55 -0300

    Change App.js to stateless component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Layout from './components/Layout/Layout';
 import BurguerBuilder from './containers/BurgerBuilder/BurguerBuilder';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <BurguerBuilder />
-      </Layout>
-    );
-  }
-}
+const App = () => (
+  <Layout>
+    <BurguerBuilder />
+  </Layout>
+);
+
+export default App;
