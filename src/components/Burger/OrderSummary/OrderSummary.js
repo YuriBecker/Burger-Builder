e09c091606bb44280589283e643abbb480c0ea1c
Yuri Becker <becker.yr@gmail.com>
2019-08-02 17:54:39 -0300

    Update orderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-  const { ingredients } = props;
+const OrderSummary = ({
+  ingredients,
+  purchaseContinued,
+  purchaseCanceled,
+  price
+}) => {
   const ingSummary = Object.keys(ingredients).map(igKey => (
     <li key={igKey}>
       <span style={{ textTransform: 'capitalize' }}>{igKey}</span>
@@ -16,13 +21,25 @@ const OrderSummary = (props) => {
       <h3>Your Order</h3>
       <p>Your burger's ingredients list:</p>
       <ul>{ingSummary}</ul>
+      <p>
+        <strong>{`Total Price: ${price.toFixed(2)}`}</strong>
+      </p>
       <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={purchaseCanceled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={purchaseContinued}>
+        Continue
+      </Button>
     </>
   );
 };
 
 OrderSummary.propTypes = {
-  ingredients: PropTypes.object.isRequired
+  ingredients: PropTypes.object.isRequired,
+  purchaseCanceled: PropTypes.func.isRequired,
+  purchaseContinued: PropTypes.func.isRequired,
+  price: PropTypes.number.isRequired
 };
 
 export default OrderSummary;
