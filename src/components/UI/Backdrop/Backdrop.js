d990f64bd8d1933a1acae92c809c55a0c756eefa
Yuri Becker <becker.yr@gmail.com>
2019-08-02 17:56:47 -0300

    Update props destructuring

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,10 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Backdrop.module.css';
 
-const Backdrop = (props) => {
-  const { show, closeModal } = props;
-  return show ? <div className={styles.Backdrop} onClick={closeModal} /> : null;
-};
+const Backdrop = ({ show, closeModal }) => (show ? (
+  <div className={styles.Backdrop} onClick={closeModal} role="dialog" />
+) : null);
 
 Backdrop.propTypes = {
   show: PropTypes.bool.isRequired,
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,23 +3,20 @@ import PropTypes from 'prop-types';
 import styles from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-  const { children, show, closeModal } = props;
-  return (
-    <>
-      <Backdrop show={show} closeModal={closeModal} />
-      <div
-        className={styles.Modal}
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0'
-        }}
-      >
-        {children}
-      </div>
-    </>
-  );
-};
+const Modal = ({ children, show, closeModal }) => (
+  <>
+    <Backdrop show={show} closeModal={closeModal} />
+    <div
+      className={styles.Modal}
+      style={{
+        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: show ? '1' : '0'
+      }}
+    >
+      {children}
+    </div>
+  </>
+);
 
 Modal.propTypes = {
   children: PropTypes.any,
